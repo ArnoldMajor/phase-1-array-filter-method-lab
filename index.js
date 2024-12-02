6ea39ae94cb2126656ae9a523e135f84431ddddd
Arnold Mwangakala <arnoldmwangakala@gmail.com>
2024-12-02 20:53:56 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(arr, name) {
+    const matches = arr.filter(function (element) {
+        return element.toLowerCase() === name.toLowerCase();
+    })
+    return matches;
+}
+
+
+// console.log(findMatching(drivers, 'Bobby'))
+
+
+function fuzzyMatch(arr, firstLetter) {
+    const matches = arr.filter(function (element) {
+        return element.slice(0, firstLetter.length).toLowerCase() === firstLetter.toLowerCase()
+    })
+    return matches;
+}
+
+// console.log(fuzzyMatch(drivers, 'Sa'))
+
+const driver = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+]
+
+function matchName(arr, name) {
+    const matches = arr.filter(function (element) {
+        return (element.name).toLowerCase() === name.toLowerCase();
+    })
+    return matches;
+}
+
+// console.log(matchName(driver, "Bobby"))
